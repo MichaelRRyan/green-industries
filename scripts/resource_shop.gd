a3f96f12635108935cedd84aa9105e266064a5b6
Jack Malone <44910666+JackMalone1@users.noreply.github.com>
2022-02-10 12:39:55 +0000

    Completed feature resource buy sell Jira[QZ-50]
    * Completed Task Jira[QZ-51]
    
    * Displaying names of the resources when shop button is pressed
    
    * Completed Task Jira[QZ-51]
    
    * Completed task jira[QZ-52] (#33)
    
    * Added in display to console for amount of money, wood and minerals so you know how much you have
    
    * Add some money to the player to be able to test properly
    
    * Updated code for pull request comments
    
    * Fixed failing test
    
    * Merge branch 'master' into Feature-QZ-50-Resource-Buy-Sell

---
+++ scripts/resource_shop.gd
@@ -0,0 +1,119 @@
+extends Node2D
+
+var data = null
+var resource_manager = null
+onready var num_wood = 0
+onready var num_minerals = 0
+onready var wood_amount : Label = $WoodAmount
+onready var minerals_amount : Label = $MineralsAmount
+const WOOD_BUY_AMOUNT = 20
+const WOOD_SELL_AMOUNT = 15
+const MINERALS_BUY_AMOUNT = 30
+const MINERALS_SELL_AMOUNT = 25
+
+
+func _ready():
+	
+	resource_manager = Utility.get_dependency("resource_manager", self, true)
+	data = Utility.get_dependency("player_data_manager", self, true).local_player_data
+	data._inventory.change_money(200)
+	_display_amounts()
+	print(data)
+
+
+func _on_Wood_1_button_down():
+	num_wood += 1
+	wood_amount.text = "Wood amount: " + str(num_wood)
+	
+
+func _on_Wood_5_button_down():
+	num_wood += 5
+	wood_amount.text = "Wood amount: " + str(num_wood)
+
+
+func _on_Wood_10_button_down():
+	num_wood += 10
+	wood_amount.text = "Wood amount: " + str(num_wood)
+
+
+func _on_Wood_All_button_down():
+	num_wood = data._inventory.get_quantity(resource_manager.get_resource_type_by_name("wood"))
+	wood_amount.text = "Wood amount: " + str(num_wood)
+
+
+func _on_Minerals_1_button_down():
+	num_minerals += 1
+	minerals_amount.text = "Minerals amount: " + str(num_minerals)
+
+
+func _on_Minerals_5_button_down():
+	num_minerals += 5
+	minerals_amount.text = "Minerals amount: " + str(num_minerals)
+
+
+func _on_Minerals_10_button_down():
+	num_minerals += 10
+	minerals_amount.text = "Minerals amount: " + str(num_minerals)
+
+
+func _on_Minerals_All_button_down():
+	num_minerals = data._inventory.get_quantity(resource_manager.get_resource_type_by_name("minerals"))
+	minerals_amount.text = "Minerals amount: " + str(num_minerals)
+
+
+func _on_Clear_button_down():
+	_reset()
+
+
+func _on_Sell_button_down():
+	var actual_wood_amount =\
+		data._inventory.get_quantity(resource_manager.get_resource_type_by_name("wood"))
+	var actual_minerals_amount =\
+		data._inventory.get_quantity(resource_manager.get_resource_type_by_name("minerals"))
+	if actual_minerals_amount >= num_minerals and actual_wood_amount >= num_wood and (num_wood > 0\
+	or num_minerals > 0):
+		var total_cost = (num_wood * WOOD_SELL_AMOUNT) + (num_minerals * MINERALS_SELL_AMOUNT)
+		data._inventory.change_money(total_cost)
+		data._inventory.\
+			remove_resources(resource_manager.get_resource_type_by_name("wood"), num_wood)
+		data._inventory.\
+			remove_resources(resource_manager.get_resource_type_by_name("minerals"), num_minerals)
+		_reset()
+		_display_amounts()
+	else:
+		print("Can't sell")
+
+
+func _on_Buy_button_down():
+	var total_cost = num_wood * WOOD_BUY_AMOUNT + num_minerals * MINERALS_BUY_AMOUNT
+	if data._inventory.get_money() >= total_cost and (num_minerals > 0 or num_wood > 0):
+		data._inventory.change_money(-total_cost)
+		data._inventory.add_resources(resource_manager.get_resource_type_by_name("wood"), num_wood)
+		data._inventory.add_resources(resource_manager.get_resource_type_by_name("minerals"),\
+			num_minerals)
+		_display_amounts()
+		_reset()
+	else:
+		print("Can't buy")
+
+
+func _display_amounts() -> void:
+	print("Total Money: " + str(data._inventory.get_money()))
+	print("Total Wood: " + str(data._inventory.get_quantity(\
+		resource_manager.get_resource_type_by_name("wood"))))
+	print("Total Minerals: " + str(\
+		data._inventory.get_quantity(resource_manager.get_resource_type_by_name("minerals"))))
+
+
+func _on_Shop_button_down():
+	visible = not visible
+	if visible:
+		_display_amounts()
+	get_tree().paused = not get_tree().paused
+	_reset()
+	
+func _reset():
+	num_wood = 0
+	num_minerals = 0
+	wood_amount.text = "Wood amount: " + str(num_wood)
+	minerals_amount.text = "Minerals amount: " + str(num_minerals)
---
+++ tests/test_resource_shop.gd
@@ -0,0 +1,93 @@
+extends "res://addons/gut/test.gd"
+
+# Name constants.
+const WOOD_RESOURCE_NAME = "wood"
+const MINERALS_RESOURCE_NAME = "minerals"
+const RESOURCE_MANAGER_NAME = "ResourceManager"
+const RESOURCE_SHOP_NAME = "ResourceShop"
+
+const TERRAIN_NAME : String = "Terrain"
+const BUILD_TOOL_NAME : String = "BuildTool"
+const LUMBERJACK_NAME : String = "Lumberjack"
+const PLAYER_DATA_NAME : String = "PlayerData"
+
+var GameplayScene = preload("res://scenes/gameplay.tscn")
+
+var _gameplay = null
+var _terrain = null
+var _build_tool = null
+var _player_data = null
+var _resource_manager = null
+var _inventory : Inventory = null
+var _resource_shop = null
+
+# ------------------------------------------------------------------------------
+func before_all() -> void:
+# Checks the gameplay scene isn't null and then adds it as a child.
+	assert_not_null(GameplayScene)
+	_gameplay = GameplayScene.instance()
+	add_child(_gameplay)
+	
+	# Finds a child node with the terrain name and ensures it's not null.
+	_terrain = find_node(TERRAIN_NAME, true, false)
+	assert_not_null(_terrain)
+	
+	# Finds a child node with the build tool name and ensures it's not null.
+	_build_tool = find_node(BUILD_TOOL_NAME, true, false)
+	assert_not_null(_build_tool)
+	
+	# Finds a child node with the player data name and ensures it's not null.
+	_player_data = find_node(PLAYER_DATA_NAME, true, false)
+	assert_not_null(_player_data)
+	
+	# Gets a the player data's inventory and ensures it's not null.
+	_inventory = _player_data._inventory
+	assert_not_null(_inventory)
+	
+	# Tries to find the resource manager and ensures it's not null.
+	_resource_manager = find_node(RESOURCE_MANAGER_NAME, true, false)
+	assert_not_null(_resource_manager)
+	
+	_resource_shop = find_node(RESOURCE_SHOP_NAME, true, false)
+	assert_not_null(_resource_shop)
+
+func before_each() -> void:
+	_inventory.clear()
+	_inventory.set_money(100)
+	_resource_shop._reset()
+# ------------------------------------------------------------------------------
+func after_all() -> void:
+	_gameplay.free()
+
+
+func test_buy_resources() -> void:
+	assert_eq(_inventory._money, 100)
+	_resource_shop._on_Minerals_1_button_down()
+	_resource_shop._on_Buy_button_down()
+	assert_eq(_inventory._money, 70)
+	
+func test_buy_not_enough_money() -> void:
+	var minerals = _resource_manager.get_resource_type_by_name(MINERALS_RESOURCE_NAME)
+	var mineral_amount = _inventory.get_quantity(minerals)
+	assert_eq(_inventory._money, 100)
+	_resource_shop._on_Minerals_10_button_down()
+	_resource_shop._on_Buy_button_down()
+	assert_eq(mineral_amount, _inventory.get_quantity(minerals))
+	assert_eq(_inventory._money, 100)
+
+func test_sell_resources() -> void:
+	var wood = _resource_manager.get_resource_type_by_name(WOOD_RESOURCE_NAME)
+	assert_not_null(wood)
+	_inventory.add_resources(wood, 5)
+	assert_eq(_inventory.get_quantity(wood), 5)
+	_resource_shop._on_Wood_5_button_down()
+	_resource_shop._on_Sell_button_down()
+	assert_eq(_inventory.get_quantity(wood), 0)
+	assert_eq(_inventory._money, 175)
+
+func test_not_enough_resources_to_sell() -> void:
+	var wood = _resource_manager.get_resource_type_by_name(WOOD_RESOURCE_NAME)
+	_resource_shop._on_Wood_5_button_down()
+	_resource_shop._on_Sell_button_down()
+	assert_eq(_inventory.get_quantity(wood), 0)
+	assert_eq(_inventory._money, 100)
